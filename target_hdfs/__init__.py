b3a29765520dbdd4791cab0659787be0d09cef2a
joaoamaral <7281460+joaopamaral@users.noreply.github.com>
2023-01-23 17:53:04 -0300

    Performance test

--- target_hdfs/__init__.py
+++ target_hdfs/__init__.py
@@ -107,7 +107,7 @@ def persist_messages(messages, config: TargetConfig):
             for message in message_buffer:
                 LOGGER.debug(f'target-parquet got message: {message}')
                 try:
-                    message = singer.parse_message(message).asdict()
+                    message = json.loads(message)
                 except json.JSONDecodeError as exc:
                     raise Exception(f'Unable to parse:\n{message}') from exc
 
