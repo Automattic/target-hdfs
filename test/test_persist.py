5cc243b867d13ef2193d375b2b1d6d15938e7c86
joaoamaral <7281460+joaopamaral@users.noreply.github.com>
2023-03-01 14:16:41 -0300

    Remove extra test

--- test/test_persist.py
+++ test/test_persist.py
@@ -186,8 +186,3 @@ class TestPersist(TestCase):
 
             self.assertEqual(len(filename), 10)
             self.assertAlmostEqual(bytes_to_mb(df.nbytes), 1, places=1)  # approx 1MB per file
-
-    def test_persist_huge(self):
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            os.makedirs(os.path.join(tmpdirname, 'test'))
-            persist_messages(self.generate_input_message(INPUT_MESSAGE_1 * 100_000), TargetConfig(f"{tmpdirname}", file_size_mb=800))
