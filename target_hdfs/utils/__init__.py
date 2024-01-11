4cba193628c177f939b876dd5e8998c68904013e
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2024-01-11 19:12:20 -0300

    Linter fix

--- target_hdfs/utils/__init__.py
+++ target_hdfs/utils/__init__.py
@@ -1,12 +1,8 @@
 import re
 
 
-def bytes_to_mb(x):
-    return x / (1024 * 1024)
-
-
-def convert_size_to_bytes(size_str):
-    """Convert a size string to bytes"""
+def convert_size_to_bytes(size_str: str) -> int:
+    """Convert a size string to bytes."""
     match = re.match(r"(\d+)([KkMmGg]?)", size_str)
     if not match:
         raise ValueError(f"Invalid size string: {size_str}")
