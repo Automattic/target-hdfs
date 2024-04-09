38712140d8c3f6f1b596bdfa62e79ad44c6c779b
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2024-04-09 14:56:09 -0300

    write file only if any record has been read.

--- target_hdfs/sinks.py
+++ target_hdfs/sinks.py
@@ -67,6 +67,7 @@ class HDFSSink(ParquetSink):
 
     def write_file(self) -> None:
         """Write a local file and upload to hdfs."""
-        self.logger.info("Total rows processed: %s", self._total_records_read)
-        super().write_file()
-        self.upload_files()
+        if self._total_records_read > 0:
+            self.logger.debug("Total rows processed: %s", self._total_records_read)
+            super().write_file()
+            self.upload_files()
