5724dcc374b970330f9db4b26dbf294122e0ec07
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2024-07-31 15:56:25 -0300

    Add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,39 @@
 """Test Configuration."""
+
+import pytest
+from pyarrow._fs import FileInfo
+
+
+@pytest.fixture(autouse=True)
+def mock_get_most_recent_file(monkeypatch):
+    def mock_get_most_recent_file(hdfs_file_path):
+        return FileInfo(path="/mock/path", size=2048)  # Example mock file
+
+    monkeypatch.setattr(
+        "target_hdfs.utils.hdfs.get_most_recent_file", mock_get_most_recent_file
+    )
+
+
+@pytest.fixture(autouse=True)
+def mock_download_from_hdfs(monkeypatch):
+    monkeypatch.setattr("target_hdfs.utils.hdfs.download_from_hdfs", lambda *args: None)
+
+
+@pytest.fixture(autouse=True)
+def mock_get_hdfs_block_size(monkeypatch):
+    def mock_get_hdfs_block_size():
+        return 10240
+
+    monkeypatch.setattr(
+        "target_hdfs.utils.hdfs.get_hdfs_block_size", mock_get_hdfs_block_size
+    )
+
+
+@pytest.fixture(autouse=True)
+def mock_hdfs_requests(monkeypatch):
+    monkeypatch.setattr("target_hdfs.sinks.read_most_recent_file", lambda *args: None)
+
+
+@pytest.fixture(autouse=True)
+def mock_time(monkeypatch):
+    monkeypatch.setattr("time.time", lambda: 1700000000)
---
+++ tests/test_e2e.py
@@ -0,0 +1,78 @@
+import json
+from io import StringIO
+from unittest.mock import patch, call
+
+import pytest
+from singer_sdk import typing as th
+from singer_sdk.testing import target_sync_test
+
+from target_hdfs.target import TargetHDFS
+
+
+@pytest.fixture(scope="session")
+def sample_config():
+    return {
+        "hdfs_destination_path": "/tmp/meltano_test",
+    }
+
+
+@patch("target_hdfs.sinks.upload_to_hdfs", return_value=None)
+def test_upload(mock_upload_to_hdfs, sample_config):
+    """Test that the target creates multiple files when the pyarrow file size limit is reached"""
+    stream_name = f"test_schema"
+    schema_message = {
+        "type": "SCHEMA",
+        "stream": stream_name,
+        "schema": {
+            "type": "object",
+            "properties": {
+                "col_a": th.StringType().to_dict(),
+                "col_b": th.StringType().to_dict(),
+                "col_c": th.StringType().to_dict(),
+                "col_d": th.StringType().to_dict(),
+                "col_e": th.StringType().to_dict(),
+                "col_f": th.StringType().to_dict(),
+                "col_g": th.StringType().to_dict(),
+                "col_h": th.StringType().to_dict(),
+            },
+        },
+    }
+    tap_output = "\n".join(
+        json.dumps(msg)
+        for msg in [schema_message]
+        + [
+            {
+                "type": "RECORD",
+                "stream": stream_name,
+                "record": {
+                    "col_a": "samplerow1",
+                    "col_b": "samplerow1",
+                    "col_c": "samplerow1",
+                    "col_d": "samplerow1",
+                    "col_e": "samplerow1",
+                    "col_f": "samplerow1",
+                    "col_g": "samplerow1",
+                    "col_h": "samplerow1",
+                },
+            }
+        ]
+        * 1000000
+    )
+
+    target_sync_test(
+        TargetHDFS(config=sample_config | {"max_pyarrow_table_size": 10}),
+        input=StringIO(tap_output),
+        finalize=True,
+    )
+
+    # 10 files are created but we should have 100 uploads
+    expected_calls = [
+        call(
+            f"output/test_schema/test_schema-20231114_221320-{i}-0.gz.parquet",
+            f"/tmp/meltano_test/test_schema/test_schema-20231114_221320-{i}-0.gz.parquet",
+        )
+        for i in range(10)
+        for _ in range(10)
+    ]
+
+    mock_upload_to_hdfs.assert_has_calls(expected_calls, any_order=True)
