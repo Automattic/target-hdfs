8804a3758cdee6432fff61630a218f5d706d7554
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2025-05-14 18:12:30 -0300

    check

--- target_hdfs/target.py
+++ target_hdfs/target.py
@@ -53,6 +53,7 @@ class TargetHDFS(TargetParquet):
         TODO: Remove after https://github.com/meltano/sdk/pull/3040 is released.
         """
         if state:  # Check if state is not empty before emit
+            self.logger.info("CUSTOM STATE EMITTED: %s", state)
             super()._write_state_message(state)
         else:
             self.logger.info("No state to write")
